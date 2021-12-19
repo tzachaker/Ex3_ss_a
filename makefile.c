0d5cc20db3ac8e5795893bf2983e240897fdbaa1
tzachaker <76492492+tzachaker@users.noreply.github.com>
2021-12-19 12:55:39 +0200

    Update makefile.c

--- makefile.c
+++ makefile.c
@@ -3,10 +3,11 @@ FLAGS= -Wall -g
 all: stringProg
 
 stringProg:main.o
-	gcc $(FLAGS) -o main main.o
+	gcc $(FLAGS) -o stringProg main.o
 main.o:main.c
 	gcc $(FLAGS) -c main.c
 
 .PHONY: clean all
 clean:
-	rm -f *.o
+	rm -f *.o stringProg
+
